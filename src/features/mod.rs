bba96893f6bcc6233f09551d00b58b13ff04b59e
kyka123 <krzysztofkasprzyk@outlook.com>
2026-06-30 11:46:48 +0200

    feat: implement computing Teichmuller numbers

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,1 +1,2 @@
 pub mod hensel_lemma;
+mod teichmuller;
---
+++ src/features/teichmuller/compute_teichmuller_numbers.rs
@@ -0,0 +1,18 @@
+use crate::features::hensel_lemma::lemma::hensel_lemma;
+use crate::features::hensel_lemma::polynomial::Polynomial;
+use crate::padics::core::PAdicNumber;
+use crate::padics::schemes::rational::RationalNumber;
+
+fn compute_teichmuller_numbers<const PRIME: u64>() -> Vec<Box<dyn PAdicNumber<PRIME>>> {
+    let mut coefficients = vec![RationalNumber::zero(), - RationalNumber::one()];
+
+    for _ in 2..PRIME {
+        coefficients.push(RationalNumber::zero());
+    }
+
+    coefficients.push(RationalNumber::one());
+
+    let p = Polynomial::new(coefficients); // x^p - x
+
+    hensel_lemma(p)
+}
