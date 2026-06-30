3699745de9dd2b99ea4d4f2484ff204e479a9231
kyka123 <krzysztofkasprzyk@outlook.com>
2026-06-30 10:13:46 +0200

    feat: implement Display trait for p-adic numbers

--- src/padics/core.rs
+++ src/padics/core.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display};
+
 pub trait PAdicNumber<const PRIME: u64> {
     fn iter(&self) -> Box<dyn PAdicIter>;
     fn clone_box(&self) -> Box<dyn PAdicNumber<PRIME>>;
@@ -5,4 +7,20 @@ pub trait PAdicNumber<const PRIME: u64> {
 
 pub trait PAdicIter: Iterator<Item = u64> {
     fn clone_box(&self) -> Box<dyn PAdicIter>;
+}
+
+impl<const PRIME: u64> Display for dyn PAdicNumber<PRIME> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const SIZE: u16 = 10;
+
+        let mut working_iter = self.iter();
+
+        let mut digits = Vec::new();
+        for n in 0..SIZE - 1 {
+            let digit = working_iter.next().unwrap_or(0);
+            digits.push(format!("{}*{}^{}", digit, PRIME, n));
+        }
+
+        write!(f, "{} + ...", digits.join(" + "))
+    }
 }
